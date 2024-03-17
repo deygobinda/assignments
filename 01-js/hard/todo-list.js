77a8076b525437665cb5c810aad49482161096fe
deygobinda <deygobinda2808@gmail.com>
2024-03-18 02:05:33 +0530

    todo-list

--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -11,6 +11,46 @@
 */
 
 class Todo {
+  constructor() {
+    this.todos = [];
+  }
+
+  add(todo) {
+    this.todos.push(todo);
+  }
+
+  remove(indexOfTodo) {
+    let newtodo = this.todos.filter((e) => e != this.todos[indexOfTodo]);
+    this.todos = newtodo;
+  }
+
+  update(index, updatedTodo) {
+    this.todos.map( e =>{
+      if(e === this.todos[index]){
+        this.todos[index] = updatedTodo
+      }
+    })
+  }
+
+  getAll(){
+    return this.todos;
+  }
+
+  get(indexOfTodo) {
+    return this.todos[indexOfTodo] || null;
+  }
+
+
+  update(index, updatedTodo) {
+    if (index < 0 || index >= this.todos.length) {
+      return; // Handle invalid index gracefully (e.g., throw an error or log a message)
+    }
+    this.todos[index] = updatedTodo;
+  }
+
+  clear(){
+    this.todos = [];
+  }
 
 }
 
